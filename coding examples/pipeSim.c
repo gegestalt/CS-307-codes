2123d7c4cc3e59fd2738ef0b0eff87d8e3baf098
Güney Ali Ünal <guneyunal@sabanciuniv.edu>
2022-10-31 21:36:46 +0000

    grep -b -b works

--- coding examples/pipeSim.c
+++ coding examples/pipeSim.c
@@ -6,12 +6,8 @@
 #include <fcntl.h>
 #include <string.h>
 #include <sys/stat.h>
-
 int main() {
-
-    
-    printf("I'm SHELL process, with PID: %d - Main command is: man -P cat ls | grep -e -A -A 1 -m 1 \n", getpid());
-    //file descriptor creation.
+    printf("I am SHELL process, with PID: %d - Main command is: man -P cat ls | grep -e -B -B 1 -m 1 \n", getpid());
     int fd[2]; pipe(fd);
     //child for man commmand 
     int pid1 = fork();
@@ -19,46 +15,25 @@ int main() {
         exit(1);
     } 
     else if (pid1 == 0) {
-
         printf("I’m MAN process, with PID: %d - My command is: man -P cat ls \n", getpid());
-
-        // Writes man process result to write-end of file descriptor
         dup2(fd[1], STDOUT_FILENO);
         execlp("man", "man", "-P", "cat", "ls", NULL);
     } 
     
-    // Returns to main "SHELL" process
-    else{
-
-        // Creates a child process for GREP process
+    else{//return shell process
         int pid2 = fork();
-
-        // Make sure that the fork is successful 
         if (pid2 < 0) {
             exit(1);
         } 
-        
-        // Child "GREP" process
         else if (pid2 == 0) {
 
-            printf("I’m GREP process, with PID: %d - My command is: grep -e -A -A 1 -m 1 \n", getpid());
-
-            // Creates output.txt file
+            printf("I’m GREP process, with PID: %d - My command is: grep -e -B -B 1 -m 1 \n", getpid());
             int filefd = open("output.txt", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-
-            // Gets man process output from read-end of file descriptor
             dup2(fd[0], STDIN_FILENO);
-
-            // Writes grep process result to output.txt
             dup2(filefd, STDOUT_FILENO);
-            execlp("grep", "grep", "-e", "-A", "-A", "1", "-m", "1", NULL);
+            execlp("grep", "grep", "-e", "-B", "-B", "1", "-m", "1", NULL);
         } 
-
         else{
-
-            //wait for child and print last msg.
-            waitpid(pid1, NULL, 0); 
-            waitpid(pid2, NULL, 0);            
             printf("I’m SHELL process, with PID: %d - execution is completed, you can find the results in output.txt \n", getpid());
         }
     }
